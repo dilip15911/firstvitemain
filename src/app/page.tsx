d26f7783e2f5c08119d2fe2b7f83beb59fcd6a31
Dilip Kumar <dilip15911@gmail.com>
2025-02-11 01:01:54 +0530

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -54,7 +54,7 @@ const App = () => {
         <CustomNavbarWrapper />
        
         {/* <Admin /> */}
-        <Signup/>
+        {/* <Signup/> */}
         {/* <AdminLogin/> */}
         {/* <GenerativeAICourses/> */}
         <MyCustomCarousel />
